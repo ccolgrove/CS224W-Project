b772fd85046c0da8e3f1c68eea1ce2d7efcb596d
Rowan Chakoumakos <rowanc@stanford.edu>
2011-11-27 13:50:51 -0800

    Fix unicode for pages so all pages can have a title

--- import/import_pages.py
+++ import/import_pages.py
@@ -3,8 +3,8 @@ import pymongo
 
 db = pymongo.Connection('localhost', 1000).wp
 db.drop_collection("pages")
-db.pages.create_index("page_id")
-db.pages.create_index("page_title")
+#db.pages.create_index("page_id")
+#db.pages.create_index("page_title")
 pageReader = csv.reader(open('page_info.csv', 'r'), delimiter=',')
 count = 0
 for row in pageReader:
@@ -12,11 +12,10 @@ for row in pageReader:
         print count
     db.pages.insert({
         "page_id": int(row[0]),
-        "page_title": row[1],
+        "page_title": row[1].decode('latin-1').encode('utf-8'),
         "page_is_redirect": bool(row[2]),
         "page_is_new": bool(row[3]),
-        "page_latest": bool(row[4]),
-        
+        "page_latest": bool(row[4]),    
         "page_len": row[5],
     })
     count += 1
