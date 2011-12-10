62ea25432b93e1bd4bf7be0cd1ae874583004d11
Rowan Chakoumakos <rowanc@stanford.edu>
2011-12-10 02:25:34 -0800

    Bash script to run on varying sizes

---
+++ run_on_varying_dataset_sizes_with_hits.sh
@@ -0,0 +1,21 @@
+#!/bin/bash   
+NUMBER="50"
+
+for NUMBER in 50 100 200 300 400 500 600 700 800
+do
+        
+CATEGORY_ID_FILE="./datasets/american_actors/aa_$NUMBER.txt"
+RANDOM_ID_FILE="./datasets/american_actors/random_$NUMBER.txt"
+
+IDS="./actor_ids.txt"
+ACTOR_ONE_HOP_IDS="./actor_one_hop_ids.txt"
+
+RESULTS_NO_HITS="./datasets/american_actors/features/level2/aa_and_random_features_$NUMBER.csv"
+HITS_RESULTS="./datasets/american_actors/features/level2/aa_and_random_HITS_$NUMBER.csv"
+RESULTS_WITH_HITS="./datasets/american_actors/features/level2/aa_and_random_features_with_HITS_$NUMBER.csv"
+
+python getFeaturesUsingDB.py $CATEGORY_ID_FILE $RANDOM_ID_FILE $RESULTS_NO_HITS $IDS $ACTOR_ONE_HOP_IDS 
+python hits2.py $HITS_RESULTS $NUMBER american_actors_categories_catids_noDuplicates.txt $CATEGORY_ID_FILE $RANDOM_ID_FILE
+python combine_csv_features.py $RESULTS_NO_HITS $HITS_RESULTS $RESULTS_WITH_HITS
+
+done
